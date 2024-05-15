d8697814419f7e2f8577a5fb63b944695aa95b31
Karthiksriram09 <karthiksriram009@gmail.com>
2024-05-16 00:51:10 +0530

    deploy

---
+++ main.js
@@ -0,0 +1,7 @@
+ var typed = new Typed(".text", {
+        strings: ["Web Developer", "BTech Student"], // Corrected "Strings" to "strings"
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
